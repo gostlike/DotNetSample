dc53be443143ee4d04b43c9a192078c21e3405b4
HarryYao <skct3038@skl.com.tw>
2016-09-12 17:01:25 +0800

    Accessor , Object Relation

---
+++ DesignPattern/ClassLibrary/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    // Class definition.
+    public class Student
+    {
+        public Student(Course course)
+        {
+            this.course = course;
+        }
+
+        #region Property,Accessor Sample Code
+        private string name ; 
+              
+        public string Name {
+            //Accessor
+            get { return name; }
+            set { }
+        }
+        
+        public Student(string stuName) {
+            this.name = stuName;
+        }
+        #endregion       
+
+        #region Relation Association Sample Code           
+        // 取得課程名稱               
+        public string getCourseName(Course course)
+        {
+            return course.Name;
+        }
+        #endregion
+
+        #region Relation Aggregation Sample Code                
+        private Course course2;        
+        // 選課        
+        public void setCourse(Course course) {
+            this.course2 = course;
+        }
+        #endregion
+
+        #region Relation Compostion Sample Code
+        // Compostion
+        private Course course = new Course();
+        #endregion
+    }
+}
---
+++ DesignPattern/ConsoleApplication/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ClassLibrary;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.accessorSample();
+        }
+
+        /// <summary>
+        /// Accessor範例 : name的存取範圍設成唯讀,只有建構子可以設定
+        /// </summary>
+        void accessorSample()
+        {
+            Student stu = new Student("Harry Yao");
+            Console.WriteLine("set stu name is {0}" , stu.Name);
+            //stu.name = "Harry Yao";
+            // You can't change Name
+            stu.Name = "Harry Yao2";
+            Console.WriteLine("You can't change the Name ,  still {0} ", stu.Name);
+            Console.Read();
+        }
+
+        void relationSample()
+        {
+            
+        }
+
+    }
+}
